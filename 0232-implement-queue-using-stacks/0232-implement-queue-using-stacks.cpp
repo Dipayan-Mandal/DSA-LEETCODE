b0e1fd70438838c78594ff709d7434785fad920b
Dipayan Mandal <104692213+Dipayan-Mandal@users.noreply.github.com>
2026-04-25 08:07:08 +0530

    Time: 0 ms (100%), Space: 9.6 MB (69.01%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,51 +1,43 @@
 class MyQueue {
 public:
-stack<int> st;
-stack<int> gt;
-    MyQueue() {
-        
-    }
-    
+    stack<int> st;
+    stack<int> gt;
+    MyQueue() {}
+
     void push(int x) {
-        st.push(x);
+        // push at bottom
+        if (st.size() == 0) {
+            st.push(x);
+            return;
+        } else {
+            while (st.size() > 0) {
+                gt.push(st.top());
+                st.pop();
+            }
+            st.push(x);
+            while (gt.size() > 0) {
+                st.push(gt.top());
+                gt.pop();
+            }
+        }
     }
-    
+
     int pop() {
-        //remove at bottom
-        while(st.size()>0)
-        {
-            gt.push(st.top());
-            st.pop();
-        }
-        int x=gt.top();
-        gt.pop();
-        while(gt.size()>0)
-        {
-            st.push(gt.top());
-            gt.pop();
-        }
+        int x=st.top();
+        st.pop();
         return x;
     }
-    
+
     int peek() {
-         //retreive at bottom
-        while(st.size()>0)
-        {
-            gt.push(st.top());
-            st.pop();
-        }
-        int x=gt.top();
-        while(gt.size()>0)
-        {
-            st.push(gt.top());
-            gt.pop();
-        }
-        return x;
+        // retreive at bottom
+        return st.top();
     }
-    
+
     bool empty() {
-        if(st.size()==0) return true;
-        else return false;
+        if (st.size() == 0)
+            return true;
+        else
+            return false;
     }
 };
 
