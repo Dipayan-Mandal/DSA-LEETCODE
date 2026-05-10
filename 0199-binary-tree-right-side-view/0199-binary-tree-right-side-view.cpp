8251a6e5fb653dcd5ce0f4f22d3c3c5701c7f033
Dipayan Mandal <104692213+Dipayan-Mandal@users.noreply.github.com>
2026-05-10 11:27:03 +0530

    Time: 0 ms (100%), Space: 14.9 MB (84.87%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,23 +12,23 @@
  */
 class Solution {
 public:
-    /*int levels(TreeNode* root) {
+    int levels(TreeNode* root) {
         if (root == NULL)
             return 0;
         return 1 + max(levels(root->left), levels(root->right));
-    }*/
-    void preorder(TreeNode* root,int n,vector<int> &ans)
+    }
+    void preorder(TreeNode* root,int level,vector<int> &ans)
     {
         if(root==NULL) return;
-        if(n==ans.size()) ans.push_back(root->val);
-        preorder(root->right,n+1,ans);
-        preorder(root->left,n+1,ans);
+         ans[level] = root->val;
+        preorder(root->left,level+1,ans);
+        preorder(root->right,level+1,ans);
     }
     
 
     vector<int> rightSideView(TreeNode* root) {
 
-        vector<int> ans;
+        vector<int> ans(levels(root), 0);
         
         preorder(root,0,ans);
         return ans;
