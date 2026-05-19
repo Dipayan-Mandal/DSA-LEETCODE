caeb290936f3973fec34dd946a8f2a3626ede019
Dipayan Mandal <104692213+Dipayan-Mandal@users.noreply.github.com>
2026-05-19 11:55:29 +0530

    Time: 0 ms (100%), Space: 11.5 MB (77.44%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,24 +3,21 @@ public:
     void sortColors(vector<int>& nums) {
         int n=nums.size();
         int lo=0;
-        int mid=0;
         int hi=n-1;
+        int mid=0;
         while(mid<=hi)
         {
-            if(nums[mid]==0)
-            {
-                swap(nums[mid],nums[lo]);
-                mid++;lo++;
-            }
-            else if(nums[mid]==1)
-            {
-                mid++;
-            }
-            else
+            if(nums[mid]==2)
             {
                 swap(nums[mid],nums[hi]);
                 hi--;
             }
+            else if(nums[mid]==0)
+            {
+                swap(nums[mid],nums[lo]);
+                mid++;lo++;
+            }
+            else mid++;
         }
     }
 };
