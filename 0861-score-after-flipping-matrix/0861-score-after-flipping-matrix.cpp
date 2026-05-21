101924447b9758a041dc16ac7a4fcfbd3826e90d
Dipayan Mandal <104692213+Dipayan-Mandal@users.noreply.github.com>
2026-05-22 00:02:53 +0530

    Time: 0 ms (100%), Space: 11.4 MB (36.99%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -1,44 +1,53 @@
 class Solution {
 public:
     int matrixScore(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        for (int i = 0; i < m; i++) {
-            if (grid[i][0] == 0)
-                for (int j = 0; j < n; j++) {
-                    if (grid[i][j] == 0) {
-                        grid[i][j] = 1;
-                    } else {
-                        grid[i][j] = 0;
-                    }
-                }
-        }
-
-        for (int j = 0; j < n; j++) {
-            int noz = 0;
-            int noo = 0;
-            for (int i = 0; i < m; i++) {
-                if (grid[i][j] == 0)
-                    noz++;
-                else
-                    noo++;
-            }
-            for (int i = 0; i < m; i++) {
-                if (noz > noo) {
-                    if (grid[i][j] == 0)
-                        grid[i][j] = 1;
+        int m=grid.size();
+        int n=grid[0].size();
+        for(int i=0;i<m;i++)
+        {
+            if(grid[i][0]==0)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(grid[i][j]==0)
+                    grid[i][j]=1;
                     else
-                        grid[i][j] = 0;
+                    grid[i][j]=0;
                 }
             }
         }
-        int sum = 0;
-        for (int i = 0; i < m; i++) {
-            int x = 1;
-            for (int j = n - 1; j >= 0; j--) {
-                sum += grid[i][j] * x;
-                x *= 2;
+        for(int j=0;j<n;j++)
+        {
+            int noz=0;
+            int noo=0;
+            for(int i=0;i<m;i++)
+            {
+                if(grid[i][j]==0)
+                noz++;
+                else
+                noo++;
+            }
+            for(int i=0;i<m;i++)
+            {
+                if(noz>noo)
+                {
+                    if(grid[i][j]==0)
+                    grid[i][j]=1;
+                    else
+                    grid[i][j]=0;
+                }
+                
+            }
+        }
+        int sum=0;
+        int x;
+        for(int i=0;i<m;i++)
+        {
+            x=1;
+            for(int j=n-1;j>=0;j--)
+            {
+                sum+=grid[i][j]*x;
+                x*=2;
             }
         }
         return sum;
