438af3ec5c027c9d36c59905c36163a8f1435755
Dipayan Mandal <104692213+Dipayan-Mandal@users.noreply.github.com>
2026-06-08 19:04:12 +0530

    Time: 19 ms (16.28%), Space: 18.5 MB (44.38%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode* a = list1;
+        ListNode* b = list2;
+        ListNode* temp=new ListNode(100);
+        ListNode* dummy=temp;
+        while(a && b)
+        {
+            if(a->val<=b->val)
+            {
+                dummy->next=a;
+                a=a->next;
+                dummy=dummy->next;
+            }
+            else
+            {
+                dummy->next=b;
+                b=b->next;
+                dummy=dummy->next;
+            }
+        }
+        if(a==NULL) dummy->next=b;
+        else dummy->next=a;
+        return temp->next;
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0) return NULL;
+        while(lists.size()>1){
+        ListNode* list1=lists[0];
+        lists.erase(lists.begin());
+        ListNode* list2=lists[0];
+        lists.erase(lists.begin());
+        ListNode* c= merge(list1,list2);
+        lists.push_back(c);
+        }
+        return lists[0];
+    }
+};
