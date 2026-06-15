b62b64e34526f4165eacaf7998b8e2a254a3d464
Dipayan Mandal <104692213+Dipayan-Mandal@users.noreply.github.com>
2026-06-15 11:11:56 +0530

    Time: 36 ms (7.4%), Space: 38.8 MB (20.07%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,66 +1,24 @@
 class Solution {
 public:
-    int bs(vector<int>& arr, int target) {
-        int x = target;
-        int low = 0;
-        int high = arr.size() - 1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (arr[mid] == x)
-                return mid;
-
-            if (arr[mid] < x)
-                low = mid + 1;
-
-            else
-                high = mid - 1;
-        }
-        return low;
-    }
+typedef pair<int,int> pi;
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        vector<int> v;
-        int n = arr.size();
-        int idx = bs(arr, x);
-        if (idx == -1) {
-            for (int i = 0; i < k; i++) {
-                v.push_back(arr[i]);
-            }
-        } else {
-            int lb, up;
-
-            if (idx < n && arr[idx] == x) {
-
-                v.push_back(arr[idx]);
-                k--;
-
-                lb = idx - 1;
-                up = idx + 1;
-            } else {
-
-                lb = idx - 1;
-                up = idx;
-            }
-            while (k--) {
-                if (up <= n - 1 && lb >= 0) {
-                    if (arr[up] - x < x - arr[lb]) {
-                        v.push_back(arr[up]);
-                        up++;
-                    } else {
-
-                        v.push_back(arr[lb]);
-                        lb--;
-                    }
-                } else if (up <= n - 1) {
-                    v.push_back(arr[up]);
-                    up++;
-                } else {
-                    v.push_back(arr[lb]);
-                    lb--;
-                }
-            }
+        unordered_map<int,int> m;
+        int n=arr.size();
+        vector<int> ans;
+       
+        
+        priority_queue<pair<int,int>> pq;
+        for(auto num: arr)
+        {
+            pq.push({abs(num - x),num});
+            if(pq.size()>k) pq.pop();
         }
-        sort(v.begin(), v.end());
-        return v;
+        while(pq.size()>0)
+        {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
