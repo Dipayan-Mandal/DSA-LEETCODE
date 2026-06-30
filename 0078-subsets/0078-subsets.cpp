7b7949ded1353dba64b03a31c05dc4470c583ae1
Dipayan Mandal <104692213+Dipayan-Mandal@users.noreply.github.com>
2026-06-30 12:15:29 +0530

    Time: 3 ms (28.17%), Space: 15.6 MB (13.89%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
-    void remove(vector<int>& nums, vector<int> ans,
-                vector<vector<int>> &finalans, int idx) {
-        if (idx >= nums.size()) {
-            finalans.push_back(ans);
-            return;
-        }
 
-        remove(nums, ans, finalans, idx+1);
+    void subset2(vector<int>& nums,int n,int idx,vector<int> ans,vector<vector<int>> &v)
+    {
+        if(idx==n){
+        v.push_back(ans);
+        return;
+        }
+        subset2(nums,n,idx+1,ans,v);
         ans.push_back(nums[idx]);
-        remove(nums, ans, finalans, idx+1);
+        subset2(nums,n,idx+1,ans,v);
+
     }
+
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
         vector<int> ans;
-        vector<vector<int>> finalans;
-        int idx = 0;
-        remove(nums, ans, finalans, idx);
-    return finalans;
-}
+        vector<vector<int>> v;
+        int n=nums.size();
+        int idx=0;
+        subset2(nums,n,idx,ans,v);
+        return v;
+    }
 };
